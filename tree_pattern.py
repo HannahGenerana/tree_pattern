2433792ca04e587e76741de4bd18dbf0476fc654
HannahGenerana <hannahgenerana@gmail.com>
2024-01-23 23:00:56 +0800

    pseudocode

---
+++ tree_pattern.py
@@ -0,0 +1,13 @@
+# Exercise 8: Print the following pattern
+
+# pseudocode
+
+# ask user to input an integer
+
+# create a loop of row with the value of the inputted integer
+
+# create an inner loop of the integer
+
+# print the integer 
+
+# print the pattern
