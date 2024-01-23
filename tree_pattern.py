2c2eb11429b69e5adb639f24dab06c4ed8754475
HannahGenerana <hannahgenerana@gmail.com>
2024-01-23 23:13:03 +0800

    print the tree pattern

--- tree_pattern.py
+++ tree_pattern.py
@@ -1,14 +1,18 @@
 # Exercise 8: Print the following pattern
 
 # pseudocode
-number = int (input("Kindly input any random number: "))
 
 # ask user to input an integer
+number = int (input("Kindly input any random number: "))
 
 # create a loop of row with the value of the inputted integer
+for i in range (number):    
 
 # create an inner loop of the integer
+    for j in range (i):   
 
 # print the integer 
+        print (i, end="")
 
 # print the pattern
+    print ()
